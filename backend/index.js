452d529f2310f2552d85029775e966182132ec96
R.M Asan Subodha Madusankha <ashansubodha8@gmail.com>
2025-01-20 22:15:47 +0530

    Edit, Delete Travel Story API

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,15 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const upload = require('./multer');
+const fs = require('fs');
+const path = require('path');
 
 const { authenticateToken } = require('./utilties');
 
 const User = require('./module/user.module');
 const TravelStory = require('./module/travelStory.module');
+
 
 mongoose.connect(config.connectionString);
 
@@ -116,6 +120,61 @@ app.get('/get-user', authenticateToken ,async (req, res) => {
     });
 });
 
+//Route to handle image upload
+app.post('/image-upload', upload.single("image"),async (req, res) => {
+    try{
+        if (!req.file) {
+            return res
+            .status(400)
+            .json({error: true, message: 'Image is required'});
+        }
+
+        const imageUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+
+        res.status(201).json({imageUrl});
+    }
+    catch (error) {
+        res.status(500).json({error: true, message: error.message});
+    }
+});
+
+//Delete an image from the uploads directory
+app.delete('/delete-image', async (req, res) => {
+    const {imageUrl} = req.body;
+
+    if(!imageUrl) {
+        return res
+        .status(400)
+        .json({error: true, message: 'Image URL is required'});
+    }
+
+    try {
+        // Extrat the file name from the imageURL
+        const filename = path.basename(imageUrl);
+
+        //delete the file from the uploads directory
+        const filePath = path.join(__dirname, 'uploads', filename);
+
+        //check if the file exists
+        if(fs.existsSync(filePath)){
+            //delete the file
+            fs.unlinkSync(filePath);
+            res.status(200).json({message: 'Image deleted successfully'});
+        }
+        else {
+            res.status(404).json({error: true, message: 'Image not found'});
+        }
+
+    } catch (error) {
+        res.status(500).json({error: true, message: error.message});
+    }
+
+});
+
+//Serve static files from the uploads and assets directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 //Add Travel Story
 app.post('/add-travel-story', authenticateToken ,async (req, res) => {
     const {title, story, visibleLocation, ImageUrl, visitedDate} = req.body;
@@ -161,8 +220,42 @@ app.get('/get-all-stories', authenticateToken ,async (req, res) => {
     }
 });
 
-//Route to handle image upload
+//Edit Travel Story
+app.post('/edit-story/:id', authenticateToken ,async (req, res) => {
+    const {id} = req.params;
+    const {title, story, visibleLocation, ImageUrl, visitedDate} = req.body;
+    const {userId} = req.user;
 
+    //validate required fields
+    if(!title || !story || !visibleLocation || !ImageUrl || !visitedDate) {
+        return res.status(400).json({error: true, message: 'All fields are required'});
+    }
+
+    //convert visibleDate from miliseconds to date object
+    const parsedVisibledDate = new Date(parseInt(visitedDate));
+
+    try {
+        // Find the travel story by id and ensure it belongs to the user
+        const travelStory = await TravelStory.findOne({_id: id, userId: userId});
+
+        if(!travelStory) {
+            return res.status(404).json({error: true, message: 'Travel story not found'});
+        }
+
+        const placeholderImageUrl = 'http://localhost:8000/assets/placeholder.jpg';
+
+        travelStory.title = title;
+        travelStory.story = story;
+        travelStory.visibleLocation = visibleLocation;
+        travelStory.ImageUrl = ImageUrl || placeholderImageUrl;
+        travelStory.visitedDate = parsedVisibledDate;
+
+        await travelStory.save();
+        res.status(200).json({story: travelStory, message: 'Travel story updated successfully'});
+    } catch (error) {
+        res.status(500).json({error: true, message: error.message});
+    }
+});
 
 app.listen(8000);
 module.exports = app;
